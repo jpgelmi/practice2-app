1aefa5bcc1a88def7fd183f786817b8aee95afb5
Juan Pablo Gelmi <jpgelmi@gmail.com>
2020-09-02 22:32:23 -0400

    url implementation

--- src/components/cardComponents/Button.js
+++ src/components/cardComponents/Button.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native'
+import {
+    StyleSheet,
+    Text,
+    View,
+    Linking,
+    TouchableOpacity
+} from 'react-native'
 
-export default function ButtonComponent() {
+export default function ButtonComponent(props) {
+    const{data} = props
 
     function onPressAction(){
-        
+        Linking.openURL(data.url)
     }
 
     return (
         <View style = {styles.container}>
-            <TouchableOpacity onPress = {}>
+            <TouchableOpacity onPress = {onPressAction}>
                 <Text style = {styles.textStyle}>
                     Comprar
                 </Text>
